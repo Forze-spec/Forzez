39893af63b28a0fdcf33ca6a9726b7e823bcf56a
Forze-spec <64695825+Forze-spec@users.noreply.github.com>
2022-06-16 14:38:44 +0300

    Наработки

--- Code/Scripts/Units/Enemies/RangeEnemies/AirShootableEnemy/AirShootableEnemyAnimatorBehaviour.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class AirShootableEnemyAnimatorBehaviour : StateMachineBehaviour {
-    public NotifyEvent AttackEnded;
-    
-    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-        AttackEnded?.Invoke();
-    }
-}
--- Code/Scripts/Units/Player/PlayerAnimatorBehaviour.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-
-public class PlayerAnimatorBehaviour : StateMachineBehaviour {
-    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-        GameManager.Instance.player.FightController.AttackEnds();
-    }
-}
